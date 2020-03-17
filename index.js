e293e9bb326600b07fd5a6b54b18775ea62deae7
olgagit123 <56512575+olgagit123@users.noreply.github.com>
2020-03-17 20:51:22 +0000

    setup

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
+/* Todo List */
 window.onload = function() {
-
-
+    //variables 
     let form = document.getElementById("form");
     let input = document.getElementById("input");
     let btn = document.getElementById("btn");
     let list = document.getElementById("list");
     let btnClr = document.getElementById("btnClr");
     let id = 1;
-    // listItem{item: "todo item", checked: false}
+    // listItem = {item: "todo item", checked: false}
     let liItem = "";
     let todoList = [];
-    // button event listener
+    //button event listener
     btn.addEventListener("click", addTodoItem);
 
-    // list event listener
+    //list event listener
     list.addEventListener("click", boxChecked);
 
-    // event listener for clear list
+    //event listener for clear list
     btnClr.addEventListener("click", clearList);
 
-    // input.addEventListener('keydown', addTodoItemlistener);
+    // input.addEventListener("keydown", addTodoItem);
+
 
 
     if (localStorage.length < 0) {
-        btnClr.style.display = "none"; //hide clear btn
+        btnClr.style.display = "none"; //hide clear btn	
         console.log("button");
     }
 
-    // checking localstorage has data
+    //checking localStorage has data
     if (localStorage.length <= 0) {
-        btnClr.style.display = "none"; //hide clear btn
+        btnClr.style.display = "none"; //hide clear btn	
+
     }
 
-    // add todo item to list
+    //add todo item to list
     function addTodoItem() {
         if (input.value === "") {
             alert("You must enter some value!");
         } else {
             if (list.style.borderTop === "") {
                 console.log("here!")
-                list.style.borderTop = "2px solid pink";
+                list.style.borderTop = "2px solid white";
                 btnClr.style.display = "inline";
             }
             let text = input.value;
-            let item = `<li id="li-${id}">${text}<input id="box-${id}"           class="checkboxes" type="checkbox"></li>`;
-
+            let item = `<li id="li-${id}">${text}<input id="box-${id}" class="checkboxes" type="checkbox"></li>`;
             list.insertAdjacentHTML('beforeend', item);
             liItem = { item: text, checked: false };
             todoList.push(liItem);
@@ -53,11 +54,10 @@ window.onload = function() {
             form.reset();
         }
     }
-
-    // adding string through style to list item
+    //adding string through style to list item
     function boxChecked(event) {
         const element = event.target;
-        if (element.type === "chechbox") {
+        if (element.type === "checkbox") {
             element.parentNode.style.textDecoration = "line-through";
             todoList = JSON.parse(localStorage.getItem("todoList"));
             todoList[element.id.split('-')[1] - 1].checked = element.checked.toString();
@@ -65,7 +65,7 @@ window.onload = function() {
         }
     }
 
-    // adding data to local storage
+    //adding data to local storage
     function addToLocalStorage() {
         if (typeof(Storage) !== "undefined") {
             localStorage.setItem("todoList", JSON.stringify(todoList));
@@ -74,9 +74,9 @@ window.onload = function() {
         }
     }
 
-    // display all todo list
+    //display all todo list
     function displayList() {
-        list.style.borderTop = "2px solid pink";
+        list.style.borderTop = "2px solid white";
         todoList = JSON.parse(localStorage.getItem("todoList"));
         todoList.forEach(function(element) {
             console.log(element.item)
@@ -84,7 +84,7 @@ window.onload = function() {
             let item = `<li id="li-${id}">${text}<input id="box-${id}" class="checkboxes" type="checkbox"></li>`;
             list.insertAdjacentHTML("beforeend", item);
 
-            // if there is a checked box, then style
+            //if there is a checked box, then style
             if (element.checked) {
                 let li = document.getElementById("li-" + id);
                 li.style.textDecoration = "line-through";
@@ -94,7 +94,7 @@ window.onload = function() {
         });
     }
 
-    // clear list event listener
+    //clear list event listener
     function clearList() {
         todoList = [];
         localStorage.clear();
